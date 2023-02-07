af81b3c2a8469e332800aedf95b633e51c5a69f5
RituSwami13 <119066033+RituSwami13@users.noreply.github.com>
2023-02-07 22:09:59 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ const Join = (props) =>{
   return(
     <div id ="join">
       {/* Access prop 'words' and print it using .join like words.join(',')*/
-   props.words.join(',')}
+   arr.props.words.join(',')}
     </div>
   )
 }
